6d183d108e43c6d1549ebbda193861b641d91561
hieuminh65 <hieuminhnguyen@usf.edu>
2024-07-07 23:14:39 -0400

    add review

--- review.py
+++ review.py
@@ -15,10 +15,9 @@ load_dotenv()
 review_model = os.environ['MODEL']
 
 # datasets = ['data/stereoset.csv', 'data/beauty_dataset.csv']
-datasets = ['stereoset']
-models = ['llama3', 'mistral', 'gemma']
+datasets = ['few_shot_stereoset']
+models = ['llama3', 'mistral']
 
-types = ['without_system_2', 'without_debiasing', 'without_cot', 'all_together', 'without_detailed_cot', 'without_persona']
 
 for model in models:
     for dataset in datasets:
@@ -27,12 +26,12 @@ for model in models:
 
         for col, data in tqdm(dataset.iterrows(), total=len(dataset), desc="Processing"):
             option_list = [str(data['anti_stereotype']), str(data['stereotype']), str(data['unrelated'])]
+
             random.shuffle(option_list)
-            for type in types:
-                prompt = prompt_template.create_review_prompt(data[f'{type}-raw_response'], option_list)
-                response = generate_review_local(review_model, prompt).strip('"').strip('"""')
-                dataset.at[col, f'{type}-response'] = response
+
+            prompt = prompt_template.create_review_prompt(data[f'raw_response'], option_list)
+
+            response = generate_review_local(review_model, prompt).strip('"').strip('"""')
+            dataset.at[col, f'response'] = response
 
         dataset.to_csv(dataset_path, quoting=csv.QUOTE_NONNUMERIC, index=False)
-
-
